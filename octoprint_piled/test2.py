daedf9b30042e3cf6f57c7822d9017bcba288a08
Quad14 <jgehring456@gmail.com>
2018-01-23 06:55:34 +0000

    Fixed test2.py

--- octoprint_piled/test2.py
+++ octoprint_piled/test2.py
@@ -2,5 +2,5 @@ import time
 import os
 while(1):
 	for i in range(0, 110, 10):
-		os.system('sudo python strip.py 10 '+str(i))
+		os.system('sudo python /home/pi/OctoPrint-PiLed/octoprint_piled/strip.py 10 '+str(i))
 		time.sleep(.5)
